e31765312c2d369e5edfabb301f26fe571fffa83
oucar1 <oucar1@ivytech.edu>
2025-03-03 10:39:48 -0500

    added deploy options

--- app.js
+++ app.js
@@ -1,14 +1,63 @@
 const express = require("express");
 const Song = require("./models/song");
 var cors = require("cors");
-
+const jwt = require("jwt-simple");
 const app = express();
+const User = require("./models/users");
+
 app.use(cors());
 
 // Middleware that parses HTTP requests with JSON body
 app.use(express.json());
 
 const router = express.Router();
+// Create a secret word that the server will use to encode and decode the token
+const secret = "supersecret";
+
+// We can create a way to add a new person to a database
+// Post
+router.post("/user", async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).json({ error: "Missing username or passwword" });
+  }
+  const newUser = await new User({
+    username: req.body.username,
+    password: req.body.password,
+    status: req.body.status,
+  });
+  try {
+    await newUser.save();
+    res.sendStatus(201); //created
+  } catch (err) {
+    res.status(400).send;
+  }
+});
+
+// Authenticate a user to sign in
+router.post("/auth", async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(401).json({ error: "Missing username or password" });
+    return;
+  }
+  //find the user in the database
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      res.status(401).json({ error: "User not found" });
+    } else {
+      //check the username and password to see if they match
+      if (user.password === req.body.password) {
+        //create a token
+        const token = jwt.encode({ username: user.username }, secret);
+        res.json({ token: token, username: user.username });
+      } else {
+        res.status(401).json({ error: "Invalid password" });
+      }
+    }
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
 
 // Get list of all songs in the database
 router.get("/songs", async (req, res) => {
@@ -64,8 +113,19 @@ router.put("/songs/:id", async (req, res) => {
   }
 });
 
+router.delete("/songs/:id", async (req, res) => {
+  try {
+    const song = await Song.findById(req.params.id);
+    console.log(song);
+    await Song.deleteOne({ _id: song._id });
+    res.sendStatus(204);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
 app.use("/api", router);
 
-app.listen(3000, () => {
+app.listen(3000 || process.env.PORT, () => {
   console.log("Server is running on http://localhost:3000");
 });
